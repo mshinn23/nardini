76f9dbe2db100779af4ebf28fb3697d512cda3e6
Jared Lalmansingh <jared.lalmansingh@wustl.edu>
2021-09-27 16:13:37 -0500

    Added new functions to nardini such that the user can perform calculations separately.

--- nardini/score_and_plot.py
+++ nardini/score_and_plot.py
@@ -27,7 +27,7 @@ def export_analysis(to_export):
     d['sum_abs_zscore_original_seq'] = list()
     d['sum_abs_zscore_scrambled_seq'] = list()
     for seq_id in to_export:
-        oseq, sseq, zplot, splot, zm, sm = to_export[seq_id]
+        oseq, sseq, index, zplot, splot, zm, sm = to_export[seq_id]
         z_abs_sum = np.sum(abs(zm))
         s_abs_sum = np.sum(abs(sm))
 
@@ -52,7 +52,7 @@ def export_analysis(to_export):
     print(f'Analysis results saved to: "{zip_filename}"')
 
 
-def calculate_and_plot(orthseqs, typeall, num_seqs, random_seed):
+def calculate_zscore_and_plot(orthseqs, typeall, num_seqs, random_seed):
     olen        = len(orthseqs)
     tlen        = len(typeall)
     zvecdb      = np.zeros((olen, tlen**2))
@@ -110,7 +110,6 @@ def calculate_and_plot(orthseqs, typeall, num_seqs, random_seed):
         num_types = len(typeall)
         reshaped_zvecdb = np.array(zvecdb[countseqs, :]).reshape((num_types, num_types))
         reshaped_zvecdbscr = np.array(zvecdbscr[countseqs, :]).reshape((num_types, num_types))
-        sum_diff = np.sum(abs(reshaped_zvecdb - reshaped_zvecdbscr))
 
         zscore_savename = f'regular-{seq_name}.png'
         zscore_scrambled_savename = f'scrambled-{seq_name}.png'
@@ -126,9 +125,74 @@ def calculate_and_plot(orthseqs, typeall, num_seqs, random_seed):
         print(f'[ SEQ: {seq_name} | 8 / 8 ] Plot of Scrambled Z-Score matrix saved as: "{zscore_scrambled_savename}".')
         print(end='\n\n')
 
-        to_export[seq_name] = (myseq, allscrseqs[idx], zscore_savename, zscore_scrambled_savename, reshaped_zvecdb, reshaped_zvecdbscr)
+        to_export[seq_name] = (myseq, allscrseqs[idx], countseqs, zscore_savename, zscore_scrambled_savename, reshaped_zvecdb, reshaped_zvecdbscr)
 
     export_analysis(to_export)
 
     end = datetime.now()
     print('Total processing time: {}'.format(end - start))
+
+
+def calculate_zscore(orthseqs, typeall, num_seqs, random_seed):
+    olen        = len(orthseqs)
+    tlen        = len(typeall)
+    zvecdb      = np.zeros((olen, tlen**2))
+    zvecdbscr   = np.zeros((olen, tlen**2))
+    countseqs   = -1
+    start       = datetime.now()
+
+    to_export = dict()
+    for seq_record in orthseqs:
+        seq_name = str(seq_record.id)
+        myseq = str(seq_record.seq)
+        fracsall = list()
+        countseqs = countseqs + 1
+
+        print(f'[ SEQ: {seq_name} | START ] Beginning analysis of FASTA sequence: "{seq_name}"...')
+
+        for type1 in typeall:
+            count = count_residues_in_sequence(myseq, type1)
+            fraction = count / len(myseq)
+            fracsall.append(fraction)
+
+        print(f'[ SEQ: {seq_name} | 1 / 4 ] Analyzing original sequence...')
+        myarr = get_org_seq_vals(myseq, typeall, fracsall)
+        print(f'[ SEQ: {seq_name} | 2 / 4 ] Analysis of original sequence complete.')
+
+        # Returns mean of scrambles, std of scrambles, all values in a number of scramble x 64 list, and all scramble sequences
+        print(f'[ SEQ: {seq_name} | 3 / 4 ] Performing analysis of {num_seqs} scrambled sequences: mean, stddev, etc...')
+        alpha, amean, avar, allscrvals, allscrseqs = get_scramble_seqs_vals(myseq, num_seqs, typeall, fracsall, random_seed)
+        print(f'[ SEQ: {seq_name} | 4 / 4 ] Statistical analysis complete.')
+
+        # Get difference of scramble from input sequence
+        difffromseq = list()
+        for x in range(0, len(allscrvals)):
+            difffromseq.append(sum(abs(myarr[0] - allscrvals[x]))) # if care about everything
+
+        # Find most similar scramble
+        val, idx = min((val, idx) for (idx, val) in enumerate(difffromseq))
+
+        # Create 8x8 matrix of original sequence
+        for x in range(0, myarr.shape[1]):
+            if myarr[0, x] == 0:
+                zvecdb[countseqs, x] = 0
+            else:
+                zvecdb[countseqs, x] = (myarr[0, x] - amean[x]) / math.sqrt(avar[x])
+
+        # Create 8x8 matrix of most similar scramble
+        for x in range(0, len(allscrvals[idx])):
+            if allscrvals[idx, x] == 0:
+                zvecdbscr[countseqs, x] = 0
+            else:
+                zvecdbscr[countseqs, x] = (allscrvals[idx, x] - amean[x]) / math.sqrt(avar[x])
+
+        num_types = len(typeall)
+        reshaped_zvecdb = np.array(zvecdb[countseqs, :]).reshape((num_types, num_types))
+        reshaped_zvecdbscr = np.array(zvecdbscr[countseqs, :]).reshape((num_types, num_types))
+        print(end='\n\n')
+
+        to_export[seq_name] = (myseq, allscrseqs[idx], countseqs, reshaped_zvecdb, reshaped_zvecdbscr)
+
+    end = datetime.now()
+    print('Total processing time: {}'.format(end - start))
+    return to_export
